ef6212ebc516e8d03a95f39f78be27a27de64e44
user <user@DESKTOP-8IVVDEF>
2021-11-05 14:18:37 +0200

    Добавьте файлы проекта.

---
+++ Data/Models/Pictures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoCard.Data.Models
+{
+    public class Pictures
+    {
+        //public int id { get; set; }
+        public string name { get; set; }
+        public string img { get; set; }
+        public string desc { get; set; }
+        public string  sizePx { get; set; }
+        public int sizeMB { get; set; }
+        public double price { get; set; }
+        public int available { get; set; }
+        public bool isFavourite { get; set; }
+        
+    }
+}
